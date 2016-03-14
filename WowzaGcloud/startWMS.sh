7aa2ec4432b54ab2868b5125f43b84005146eedf
Peter Banik <peter@prioritylane.com>
2016-03-15 03:53:40 +0800

    fixed JAVA_OPTS to make remote debugging work

--- WowzaGcloud/startWMS.sh
+++ WowzaGcloud/startWMS.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+export JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044"
+
 /Library/WowzaStreamingEngine/java/bin/java  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1044 -Xmx4000M -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -server -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote=true -Dcom.wowza.wms.runmode=standalone -Dcom.wowza.wms.native.base=osx -Dcom.wowza.wms.AppHome=/Library/WowzaStreamingEngine -Dcom.wowza.wms.ConfigURL= -Dcom.wowza.wms.ConfigHome=/Library/WowzaStreamingEngine -cp /Library/WowzaStreamingEngine/bin/wms-bootstrap.jar com.wowza.wms.bootstrap.Bootstrap start
